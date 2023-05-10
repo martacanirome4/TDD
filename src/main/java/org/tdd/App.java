40cbf5565b0389ae8d8782b06643e510433b167b
MartaCanino <martacaninoromero@gmail.com>
2023-05-10 13:05:46 +0200

    Terminar primer test pasa con lo minimo

--- src/main/java/org/tdd/App.java
+++ src/main/java/org/tdd/App.java
@@ -1,13 +1,13 @@
 package org.tdd;
 
+import java.util.ArrayList;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public int SumN (int n) {
+        return n;
     }
 }
--- src/test/java/org/tdd/AppTest.java
+++ src/test/java/org/tdd/AppTest.java
@@ -1,20 +1,32 @@
 package org.tdd;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
 import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+public class AppTest {
+
+    App myApp;
+    @Before
+    public void setup(){
+        myApp = new App();
+    }
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void SumaNparaUno() {
+        int resultado = myApp.SumN(1);
+        assertTrue( resultado == 1 );
     }
 }
